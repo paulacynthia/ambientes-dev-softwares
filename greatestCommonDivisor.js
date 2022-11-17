20e8a847c316e558640792d656692647577170ca
paulacynthia <pcynthia013@gmail.com>
2022-11-17 13:40:45 -0300

    remove node_modules

--- greatestCommonDivisor.js
+++ greatestCommonDivisor.js
@@ -1,30 +1,30 @@
 function greatestCommonDivisor(numberOne, numberTwo) {
-  var bigger;
-  var GCD = 1;
+  var bigger
+  var GCD = 1
 
-  var valueFinalOne = numberOne;
-  var valueFinalTwo = numberTwo;
+  var valueFinalOne = numberOne
+  var valueFinalTwo = numberTwo
 
-  bigger = numberOne;
+  bigger = numberOne
 
   if (numberOne > numberTwo) {
-    bigger = numberOne;
+    bigger = numberOne
   } else if (numberOne < numberTwo) {
-    bigger = numberTwo;
+    bigger = numberTwo
   } else {
-    return "Os números são iguais!";
+    return 'Os números são iguais!'
   }
 
   for (let i = 2; i <= bigger; i++) {
     while (numberOne % i == 0 && numberTwo % i == 0) {
-      numberOne = numberOne / i;
-      numberTwo = numberTwo / i;
+      numberOne = numberOne / i
+      numberTwo = numberTwo / i
 
-      GCD = GCD * i;
+      GCD = GCD * i
     }
   }
 
-  return `MDC (${valueFinalOne},${valueFinalTwo}) = ${GCD}`;
+  return `MDC (${valueFinalOne},${valueFinalTwo}) = ${GCD}`
 }
 
-module.exports = greatestCommonDivisor;
+module.exports = greatestCommonDivisor
