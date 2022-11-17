9bde9764629756ac3800e503eb73680f7360f467
paulacynthia <pcynthia013@gmail.com>
2022-11-17 13:43:37 -0300

    change files

--- isPrime.js
+++ isPrime.js
@@ -1,19 +1,19 @@
 function isPrime(number) {
-  let isPrime = true;
+  let isPrime = true
 
   if (number < 0) {
-    return "Por favor, insira um número positivo.";
+    return 'Por favor, insira um número positivo.'
   }
 
   for (let j = 2; j < number; j++) {
     if (number % j == 0) {
-      return number + " não é um número primo";
-      isPrime = false;
-      break;
+      return number + ' não é um número primo'
+      isPrime = false
+      break
     }
   }
-  if (isPrime) return number + " é um número primo";
-  isPrime = true;
+  if (isPrime) return number + ' é um número primo'
+  isPrime = true
 }
 
-module.exports = isPrime;
+module.exports = isPrime
